a8ade747feea04d21dce9daaa56d55ea17e48e61
blacksky3 <blacksky3@tuta.io>
2022-06-02 20:06:30 -0400

    fix

--- build.sh
+++ build.sh
@@ -1,15 +1,28 @@
+# _     _            _        _          _____
+#| |__ | | __ _  ___| | _____| | ___   _|___ /
+#| '_ \| |/ _` |/ __| |/ / __| |/ / | | | |_ \
+#| |_) | | (_| | (__|   <\__ \   <| |_| |___) |
+#|_.__/|_|\__,_|\___|_|\_\___/_|\_\\__, |____/
+#                                  |___/
+
+#Maintainer: blacksky3 <blacksky3@tuta.io> <https://github.com/blacksky3>
+
 #!/bin/bash
+
+source=$(pwd)
+
+echo "${source}"
 
 # build normal package with GCC
 
 # gcc
 
-cd 5.18/pds && makepkg -s && rm -rf pkg/ src/ && cd ..
+cd 5.18/pds && makepkg -s && rm -rf pkg/ src/ && cd ${source}
 
-cd 5.18/bmq && makepkg -s && rm -rf pkg/ src/ && cd ..
+cd 5.18/bmq && makepkg -s && rm -rf pkg/ src/ && cd ${source}
 
 # clang
 
-#cd 5.18/pds && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
+#cd 5.18/pds && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ${source}
 
-#cd 5.18/bmq && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
+#cd 5.18/bmq && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ${source}
